583cdfdcb6a9a936ce181299e61a1cbf99bb14c1
hardikanbati <hardikambati69@gmail.com>
2024-03-30 12:39:20 +0530

    added doublt linked list

---
+++ doublyLinkedList.py
@@ -0,0 +1,113 @@
+# Doubly Linked List
+
+class Node:
+    """
+    creates a node with data and next
+    """
+    def __init__(self, data):
+        self.data=data
+        self.prev=None
+        self.next=None
+
+
+class LinkedList:
+    """
+    Linked List class
+    """
+
+    def __init__(self):
+        self.head=None
+        self.tail=None
+
+    
+    def insert_end(self, data):
+        """
+        inserts a node at at end
+        """
+        new_node=Node(data)
+
+        if (self.head == None and self.tail == None):
+            self.head=new_node
+            self.tail=new_node
+        else:
+            new_node.prev=self.tail
+            self.tail.next=new_node
+            self.tail=new_node
+
+    
+    def insert_start(self, data):
+        """
+        inserts a node at start
+        """
+        new_node=Node(data)
+
+        if (self.head == None and self.tail == None):
+            self.head=new_node
+            self.tail=new_node
+        else:
+            new_node.next=self.head
+            self.head.prev=new_node
+            self.head=new_node
+
+
+    def remove_first_node(self):
+        """
+        delete a node at first position
+        """
+        # no node
+        if (self.head == None):
+            return
+        # single node
+        if (self.head.next == None):
+            self.head=None
+            self.tail=None
+            return
+        
+        self.head=self.head.next
+        self.head.prev=None
+
+    
+    def remove_last_node(self):
+        """
+        delete a node at last position
+        """
+        # no node
+        if (self.tail == None):
+            return
+        # single node
+        if (self.tail.prev == None):
+            self.head=None
+            self.tail=None
+        self.tail=self.tail.prev
+        self.tail.next=None
+
+    
+    def print_list(self, flow: str='forward'):
+        """
+        prints the data of each node
+        :flow - ['forward', 'backward']
+        """
+        if (flow == 'backward'):
+            curr=self.tail
+            while(curr!=None):
+                print(curr.data, end=' <-> ')
+                curr=curr.prev
+        else:
+            curr=self.head
+            while(curr!=None):
+                print(curr.data, end=' <-> ')
+                curr=curr.next
+
+
+if __name__ == '__main__':
+
+    l = LinkedList()
+
+    l.insert_end(1)
+    l.insert_end(2)
+    l.insert_end(3)
+
+    # l.remove_first_node()
+    l.remove_last_node()
+
+    l.print_list()
